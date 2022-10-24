a837315bae117027d578d51c007c6a1b641fe00e
Dylan Oliva <djoliva@cpp.edu>
2022-10-23 19:46:45 -0700

    implemented options 2 and 3

--- number-guessing.c
+++ number-guessing.c
@@ -8,3 +8,35 @@
 //re ask them options 1 to 3
 
 //3 - to exit the program
+
+int main(){
+    int exit = 0;
+    int MAX_VAL = 10;
+    char option;
+
+    while(exit == 0){
+        printf("Choose an option 1 to 3: \n");
+        printf("1 - Play the game\n");
+        printf("2 - Change the max number\n");
+        printf("3 - Exit");
+        option = getChar();
+        getChar();
+
+        while (option == '1')
+        {
+            
+        }
+        
+        if(option == '2'){
+            char temp;
+            printf("What would you like the max value to be\n");
+            temp = getChar();
+            getChar();
+            MAX_VAL = atof(temp);
+        }
+
+        if(option == '3'){
+            exit = option;
+        }
+    }
+}
