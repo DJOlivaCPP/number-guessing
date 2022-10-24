aecb1b2bb51a5e91ca9ff0f94d9408545675550a
Dylan Oliva <djoliva@cpp.edu>
2022-10-23 19:28:06 -0700

    outline

---
+++ number-guessing.c
@@ -0,0 +1,10 @@
+//first we ask the player which option they want
+
+//1 - plays the game
+//will run a loop uuntil the player has guessed the correct number
+
+//2 - change the range
+//checks if the inputted value is a non-negative
+//re ask them options 1 to 3
+
+//3 - to exit the program
